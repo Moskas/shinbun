17dd1e570532292a5589fb8b8902467c318136ee
Moskas <minemoskas@gmail.com>
2024-04-18 21:26:10 +0200

    Applied the latest nix formatter to the flake.nixc

--- flake.nix
+++ flake.nix
@@ -6,33 +6,53 @@
     naersk.url = "github:nix-community/naersk";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    self,
-    flake-utils,
-    naersk,
-    nixpkgs,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = (import nixpkgs) {inherit system;};
-      naersk' = pkgs.callPackage naersk {};
-    in {
-      defaultPackage = naersk'.buildPackage {
-        src = ./.;
-        nativeBuildInputs = with pkgs; [pkg-config rustc];
-        buildInputs = with pkgs; [openssl];
-      };
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) { inherit system; };
+        naersk' = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            rustc
+          ];
+          buildInputs = with pkgs; [ openssl ];
+        };
 
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustc
-          cargo
-	  cargo-watch
-          rust-analyzer
-          rustPackages.clippy
-          rustfmt
-          openssl
-          pkg-config
-        ];
-      };
-    });
+        packages = {
+          check = naersk'.buildPackage {
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustc
+            ];
+            buildInputs = with pkgs; [ openssl ];
+            mode = "check";
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            cargo-watch
+            rust-analyzer
+            rustPackages.clippy
+            rustfmt
+            openssl
+            pkg-config
+          ];
+        };
+      }
+    );
 }
